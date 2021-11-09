eafe5ba0af88c68ecb104f2b4dd96133ce46d5df
ragishabaabte <ragisha@baabte.com>
2021-11-09 10:48:50 +0530

    up2

--- myapp/urls.py
+++ myapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views 
 urlpatterns = [
    path('hello',views.fnHello),
-   path('sample',views.fnSample),
+   path('sample/',views.fnSample,name="sample"),
    path('sample2',views.fnSample2)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . models import *
 # Create your views here.
 
 def fnHello(request):
     return HttpResponse("hello i am Ragisha")
 
 def fnSample(request):
+    if request.method=='POST':
+        name=request.POST['n1']
+        age=request.POST['n2']
+        usOBJ=sample(name=name,age=age)
+        usOBJ.save()
     return render(request, 'sample.html')
 def fnSample2(request):
     return render(request,'sample2.html')
